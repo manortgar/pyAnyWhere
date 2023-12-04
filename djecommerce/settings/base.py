9713e3343dbd316822a63cc739a838f5048c5634
Manuel Ortega <80253414+manortgar@users.noreply.github.com>
2023-12-04 15:15:02 +0100

    Update base.py

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
 
 SECRET_KEY = config('SECRET_KEY')
-
+ALLOWED_HOSTS = ['.vercel.app']
 
 INSTALLED_APPS = [
     'core',
